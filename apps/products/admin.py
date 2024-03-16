9453b62eb3f5bb2a116844c10cd8ee48f7d866f6
Ramazan <zr12169@auca.kg>
2024-03-16 22:05:23 +0600

    media url

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 
+from .models import ProductParameter, Product
+
 admin.site.unregister(User)
 admin.site.unregister(Group)
 
+
+class ProductParameterInline(admin.TabularInline):
+    model = ProductParameter
+    fields = ('key','value')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = (ProductParameterInline, )
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -32,3 +34,6 @@ urlpatterns = [
     path('api/v1/', include(apipatterns)),
     # path('', include(docpatterns)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
