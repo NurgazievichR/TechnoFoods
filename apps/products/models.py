4ac77447d342d361faed8fb97caa3a3b8fd562c0
Ramazan <zr12169@auca.kg>
2024-03-17 16:42:38 +0600

    added product images

--- apps/products/models.py
+++ apps/products/models.py
@@ -10,7 +10,6 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='products')
     popular = models.BooleanField(default = False)
     views = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name = 'productsOfCategory')
